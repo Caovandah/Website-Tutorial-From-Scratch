47cd80379460fe81febd72ff5169667588232085
caovandanh <caovandanh2002@gmail.com>
2022-11-22 13:10:51 +0700

    Upload: Home Hero

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HeroImg from '../components/heroimg/HeroImg';
 import Navbar from '../components/navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <HeroImg />
         </div>
     );
 };
