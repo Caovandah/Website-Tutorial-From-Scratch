ef83bd197fdbb325b48065b4036d9e5d81e7748c
caovandanh <caovandanh2002@gmail.com>
2022-11-22 11:25:03 +0700

    Upload: Creating Route Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './index.css';
 
+import Home from './router/Home';
+import About from './router/About';
+import Project from './router/Project';
+import Contact from './router/Contact';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
     return (
-        <div>
-            <h1>This is app.js</h1>
-        </div>
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/project" element={<Project />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </>
     );
 }
 
