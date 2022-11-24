fcc16dcc7cedeb31bf4c1f1892d8ba90232fa447
caovandanh <caovandanh2002@gmail.com>
2022-11-24 12:25:12 +0700

    Upload: Dynamic Card Component

--- src/router/Project.js
+++ src/router/Project.js
@@ -3,12 +3,14 @@ import Footer from '../components/footer/Footer';
 import HeroImg2 from '../components/heroimg2/HeroImg2';
 import Navbar from '../components/navbar/Navbar';
 import PricingCard from '../components/pricingcard/PricingCard';
+import Work from '../components/Work';
 
 const Project = () => {
     return (
         <div>
             <Navbar />
             <HeroImg2 heading="PROJECTS." text="Some of my most recent works." />
+            <Work />
             <PricingCard />
             <Footer />
         </div>
