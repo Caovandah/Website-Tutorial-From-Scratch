b781e8c4c7e2e34c55908a69ac54ecf0f5bdb69f
caovandanh <caovandanh2002@gmail.com>
2022-11-24 09:56:01 +0700

    Upload: Footer

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/footer/Footer';
 import HeroImg from '../components/heroimg/HeroImg';
 import Navbar from '../components/navbar/Navbar';
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Navbar />
             <HeroImg />
+            <Footer />
         </div>
     );
 };
