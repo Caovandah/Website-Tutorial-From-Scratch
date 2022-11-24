a8d2fa70ee545e67afabbe4ccc617394ec81f8ad
caovandanh <caovandanh2002@gmail.com>
2022-11-24 15:27:53 +0700

    Upload: Contact Form Component

--- src/router/Contact.js
+++ src/router/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from '../components/footer/Footer';
+import Form from '../components/form/Form';
 import HeroImg2 from '../components/heroimg2/HeroImg2';
 import Navbar from '../components/navbar/Navbar';
 
@@ -8,6 +9,7 @@ const Contact = () => {
         <div>
             <Navbar />
             <HeroImg2 heading="CONTACT." text="Lets have a chat." />
+            <Form />
             <Footer />
         </div>
     );
--- src/router/Home.js
+++ src/router/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../components/footer/Footer';
 import HeroImg from '../components/heroimg/HeroImg';
 import Navbar from '../components/navbar/Navbar';
+import Work from '../components/Work';
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <HeroImg />
+            <Work />
             <Footer />
         </div>
     );
