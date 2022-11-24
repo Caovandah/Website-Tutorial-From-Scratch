0cc81a08cd41eb32332c6a96b8c269430ae1dac3
caovandanh <caovandanh2002@gmail.com>
2022-11-24 11:06:13 +0700

    Upload: Pricing Card

--- src/router/Project.js
+++ src/router/Project.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../components/footer/Footer';
 import HeroImg2 from '../components/heroimg2/HeroImg2';
 import Navbar from '../components/navbar/Navbar';
+import PricingCard from '../components/pricingcard/PricingCard';
 
 const Project = () => {
     return (
         <div>
             <Navbar />
             <HeroImg2 heading="PROJECTS." text="Some of my most recent works." />
+            <PricingCard />
             <Footer />
         </div>
     );
